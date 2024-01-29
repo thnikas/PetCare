e8543cdceda44053cb247b26c4acfa8bf5f74c11
thnikas <8nikas@gmail.com>
2024-01-30 00:05:02 +0100

    change auth

--- lib/session.ts
+++ lib/session.ts
@@ -1,5 +1,4 @@
 import { getServerSession } from "next-auth/next";
-import { NextAuthOptions, User } from "next-auth";
 import { AdapterUser } from "next-auth/adapters";
 import GoogleProvider from "next-auth/providers/google";
 import jsonwebtoken from 'jsonwebtoken'
